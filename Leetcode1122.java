1e1c6403c667bfdf33fbca3fe56e494e582f16f2
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-06-11 14:22:12 -0400

    Update Leetcode1122.java
    Added question description

--- Leetcode1122.java
+++ Leetcode1122.java
@@ -1,3 +1,9 @@
+/*
+ * Relative sort array 
+ * Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+ * Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. 
+ * Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+ */
 class Leetcode1122 {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         Map<Integer, Integer> countMap = new HashMap<>(); 
