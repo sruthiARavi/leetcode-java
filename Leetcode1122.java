96cf9378d8ec14b8e1fff75e2231ec0e682d19f6
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-06-11 14:18:02 -0400

    Create Leetcode1122.java
    Solution for leetcode #1122

---
+++ Leetcode1122.java
@@ -0,0 +1,30 @@
+class Leetcode1122 {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> countMap = new HashMap<>(); 
+        List<Integer> remaining = new ArrayList<>(); 
+        List<Integer> result = new ArrayList<>(); 
+
+        for(int value : arr2) {
+            countMap.put(value, 0); 
+        }
+
+        for(int value : arr1) {
+            if(countMap.containsKey(value)) {
+                countMap.put(value, countMap.get(value)+1); 
+            } else {
+                remaining.add(value); 
+            }
+        }
+
+        for(int value : arr2) {
+            for(int j=0; j<countMap.get(value); j++) {
+                result.add(value); 
+            }
+        }
+
+        Collections.sort(remaining); 
+        result.addAll(remaining); 
+
+        return result.stream().mapToInt(Integer :: intValue).toArray(); 
+    }
+}
