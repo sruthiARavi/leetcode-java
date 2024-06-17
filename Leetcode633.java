6278259918f07d44e8390e2246e45b522c5c1640
sruthiARavi <166052788+sruthiARavi@users.noreply.github.com>
2024-06-17 18:48:12 -0400

    Update Leetcode633.java
    Added binary search approach in addition to the Math.sqrt approach

--- Leetcode633.java
+++ Leetcode633.java
@@ -3,13 +3,43 @@
  * Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
  */
 class Leetcode633 {
-    public boolean judgeSquareSum(int c) {
-        for(long a=0; a*a <=c; a++) {
-            double b = Math.sqrt(c-a*a); 
-            if(b == (int) b) {
-                return true; 
+    // binary search - same complexity but worse space complexity 
+    boolean doesNumExist(long min, long max, long b_square) {
+        while (min <= max) {
+            long mid = min + (max - min) / 2;
+            long product = mid * mid;
+            if (product == b_square) {
+                return true;
+            }
+            if (product < b_square) {
+                return doesNumExist(mid + 1, max, b_square);
+            } else {
+                return doesNumExist(min, mid - 1, b_square);
             }
         }
-        return false; 
+        return false;
+    }
+
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            long b_square = c - (a * a);
+            // binary search if there exists b in [0,b_square] such that b^2 = b_square
+            boolean numExists = doesNumExist(0, b_square, b_square);
+            if (numExists) {
+                return true;
+            }
+        }
+        return false;
+
+        // Math.sqrt approach - Has better space complexity
+        /*
+         * for (long a = 0; a * a <= c; a++) {
+         * double b = Math.sqrt(c - a * a);
+         * if (b == (int) b) {
+         * return true;
+         * }
+         * }
+         * return false;
+         */
     }
 }
