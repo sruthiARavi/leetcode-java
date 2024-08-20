29f30838c6041983e82b6983f6de7189b2a6e827
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-20 16:03:51 -0400

    Create Leetcode88.java

---
+++ leetcode-top-150/Leetcode88.java
@@ -0,0 +1,49 @@
+/*
+ * Merge Sorted Array
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+ * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
+
+class Leetcode88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        if (n == 0) {
+            return;
+        }
+
+        if (m == 0) {
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+        }
+
+        /*
+         * Implement similar to merge sort but go in reverse
+         * since elts are 0 in the latter half of nums1
+         */
+
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        if (j >= 0) {
+            while (j >= 0 && k >= 0) {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+
+    }
+}
