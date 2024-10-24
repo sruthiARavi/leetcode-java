c0f77a8461d6056ee7f13b69dec1169db6d6a652
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-10-23 23:05:22 -0400

    Delete MySQL directory

--- MySQL/Leetcode1757.sql
+++
@@ -1,6 +0,0 @@
-# https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
-# Write your MySQL query statement below
-SELECT product_id
-FROM   products
-WHERE  low_fats = 'Y'
-       AND recyclable = 'Y' 
