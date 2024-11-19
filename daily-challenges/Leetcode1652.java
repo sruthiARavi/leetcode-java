4b42dca250af32ca8b66b28cc5ec7f0581196bc6
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-11-19 01:20:23 -0500

    Create Leetcode1652.java

---
+++ daily-challenges/Leetcode1652.java
@@ -0,0 +1,35 @@
+/*
+ * 1652. Defuse the Bomb
+ * You have a bomb to defuse, and your time is running out! 
+ * Your informer will provide you with a circular array code of length of n and a key k.
+ * To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+   ** If k > 0, replace the ith number with the sum of the next k numbers.
+   ** If k < 0, replace the ith number with the sum of the previous k numbers.
+   ** If k == 0, replace the ith number with 0.
+ * As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+ * Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+ */
+class Leetcode1652 {
+    public int[] decrypt(int[] code, int k) {
+        // Using sliding window approach
+        int[] result = new int[code.length];
+        if (k == 0) {
+            Arrays.fill(result, 0);
+            return result;
+        }
+        int startIndex = k > 0 ? 1 : code.length - Math.abs(k);
+        int endIndex = k > 0 ? k : code.length - 1;
+        int kSum = 0;
+        for (int i = startIndex; i <= endIndex; i++) {
+            kSum += code[i];
+        }
+        for (int i = 0; i < code.length; i++) {
+            result[i] = kSum;
+            kSum -= code[(startIndex) % code.length];
+            kSum += code[(endIndex + 1) % code.length];
+            startIndex++;
+            endIndex++;
+        }
+        return result;
+    }
+}
