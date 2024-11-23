2100b3aa48b5a2fc6ed05b83da8e45deff4800bb
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-11-22 23:17:28 -0500

    Create Leetcode35.java

---
+++ algorithms/easy/Leetcode35.java
@@ -0,0 +1,24 @@
+/*
+ * 35. Search Insert Position
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+ * If not, return the index where it would be if it were inserted in order.
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+class Leetcode35 {
+    public int searchInsert(int[] nums, int target) {
+        //Binary search 
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
