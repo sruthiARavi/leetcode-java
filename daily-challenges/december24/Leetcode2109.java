f00a2dd272a58eeb64892eda999c2a8869e1e208
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-12-02 19:19:51 -0500

    Update Leetcode2109.java

--- daily-challenges/december24/Leetcode2109.java
+++ daily-challenges/december24/Leetcode2109.java
@@ -1,5 +1,5 @@
 /*
- * 
+ * 2109. Adding Spaces to a String
  * You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. 
  * Each space should be inserted before the character at the given index.
    ** For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], 
