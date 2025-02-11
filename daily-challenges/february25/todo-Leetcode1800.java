345620e6730f995062908505dc11f949e2c18bce
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2025-02-10 23:54:43 -0500

    Update todo-Leetcode1800.java

--- daily-challenges/february25/todo-Leetcode1800.java
+++ daily-challenges/february25/todo-Leetcode1800.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        // Below strategy only works if strictly positive nums are present in the array
+        int maxSum = 0;
+        int currentSubArraySum = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] <= nums[i - 1]) {
+                maxSum = Math.max(currentSubArraySum, maxSum);
+                currentSubArraySum = 0;
+            }
+            currentSubArraySum += nums[i];
+        }
+        return Math.max(currentSubArraySum, maxSum);
+    }
+}
