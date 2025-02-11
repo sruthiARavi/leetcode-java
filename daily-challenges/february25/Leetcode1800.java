f41a2aeb11095a9ee5c3ff424be355d1d7943881
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2025-02-10 23:55:38 -0500

    Update and rename todo-Leetcode1800.java to Leetcode1800.java

--- daily-challenges/february25/Leetcode1800.java
+++ daily-challenges/february25/Leetcode1800.java
@@ -1,4 +1,11 @@
-class Solution {
+/*
+ * 
+ * Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+ * A subarray is defined as a contiguous sequence of numbers in an array.
+ * A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. 
+ * Note that a subarray of size 1 is ascending.
+ */
+class Leetcode1800 {
     public int maxAscendingSum(int[] nums) {
         // Below strategy only works if strictly positive nums are present in the array
         int maxSum = 0;
