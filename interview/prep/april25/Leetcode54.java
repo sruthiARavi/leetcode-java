b024caf8716add72ea09417a944d09740b3ed80b
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2025-04-28 15:37:55 -0400

    Create Leetcode54.java

---
+++ interview/prep/april25/Leetcode54.java
@@ -0,0 +1,92 @@
+/*
+ * 54. Spiral Matrix
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ * Constraints:
+   ** m == matrix.length
+   ** n == matrix[i].length
+   ** 1 <= m, n <= 10
+   ** -100 <= matrix[i][j] <= 100
+ */
+
+class Leetcode54 {
+
+    enum Direction {
+        LEFT, RIGHT, UP, DOWN
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+
+        Direction dir = Direction.RIGHT;
+        int row = 0, col = 0;
+
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
+        boolean[][] visited = new boolean[rowLen][colLen];
+        
+        while (true) {
+            //mark visited as true 
+            visited[row][col] = true;
+            ans.add(matrix[row][col]);
+
+            int loopCnt = 0;
+            boolean canMoveOn = false;
+
+            while (loopCnt < 4) { //try all four directions
+                if (dir == Direction.RIGHT) {
+                    int tempCol = col + 1;
+                    if (tempCol == colLen || visited[row][tempCol]) {
+                        //change direction; 
+                        dir = Direction.DOWN;
+                        loopCnt++;
+                    } else {
+                        col = tempCol;
+                        canMoveOn = true;
+                        break;
+                    }
+                }
+
+                if (dir == Direction.LEFT) {
+                    int tempCol = col - 1;
+                    if (tempCol < 0 || visited[row][tempCol]) {
+                        dir = Direction.UP;
+                        loopCnt++;
+                    } else {
+                        col--;
+                        canMoveOn = true;
+                        break;
+                    }
+                }
+
+                if (dir == Direction.DOWN) {
+                    int tempRow = row + 1;
+                    if (tempRow == rowLen || visited[tempRow][col]) {
+                        dir = Direction.LEFT;
+                        loopCnt++;
+                    } else {
+                        row++;
+                        canMoveOn = true;
+                        break;
+                    }
+                }
+
+                if (dir == Direction.UP) {
+                    int tempRow = row - 1;
+                    if (tempRow < 0 || visited[tempRow][col]) {
+                        dir = Direction.RIGHT;
+                        loopCnt++;
+                    } else {
+                        row--;
+                        canMoveOn = true;
+                        break;
+                    }
+                }
+            }
+            if (!canMoveOn) {
+                break;
+            }
+        }
+        return ans;
+    }
+
+}
